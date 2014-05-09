22aaf39e676eded926a3435222933f439f3f4ca8
Andreas Heigl <andreas@heigl.org>
2014-05-09 10:17:55 +0200

    Adds location search in Map

--- src/module/Phpug/public/js/phpug.js
+++ src/module/Phpug/public/js/phpug.js
@@ -56,6 +56,13 @@ var RedIcon = L.Icon.Default.extend({
 
 var redIcon = new RedIcon();
 
+
+new L.Control.GeoSearch({
+    provider: new L.GeoSearch.Provider.OpenStreetMap(),
+    position: 'topcenter',
+    showMarker: false
+}).addTo(map);
+
 var createSelector = function(data){
     for (i in data) {
         item = data[i];
