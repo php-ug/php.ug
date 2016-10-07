c1568f82605781efbf5bdfbc6327fb28a37f644d
Andreas Heigl <andreas@heigl.org>
2016-10-07 22:26:07 +0200

    Adds some whitespace

--- src/module/Phpug/src/Phpug/Event/NotifyAdminListenerFactory.php
+++ src/module/Phpug/src/Phpug/Event/NotifyAdminListenerFactory.php
@@ -1,14 +1,17 @@
 <?php
 /**
  * Copyright (c) Andreas Heigl<andreas@heigl.org>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,7 +24,6 @@
  * @copyright Andreas Heigl
  * @license   http://www.opensource.org/licenses/mit-license.php MIT-License
  * @since     07.10.2016
- * @link      http://github.com/heiglandreas/php.ug
  */
 
 namespace Phpug\Event;
